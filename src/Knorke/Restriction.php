69b9198f3fe15999d4bb30b791019d10d2b4f28f
Konrad Abicht <k.abicht@googlemail.com>
2017-01-13 15:01:47 +0100

    Restriction: removed data blank options

--- src/Knorke/Restriction.php
+++ src/Knorke/Restriction.php
@@ -18,20 +18,22 @@ class Restriction
         $this->store = $store;
     }
 
-    public function getRestrictionsForResource($resourceUri, array $dataBlankOptions = array())
+    /**
+     * @param string $resourceUri
+     */
+    public function getRestrictionsForResource($resourceUri)
     {
-        $options = array_merge(
-            array('use_prefixed_predicates' => true, 'use_prefixed_objects' => true),
-            $dataBlankOptions
-        );
-        $blank = new DataBlank($this->commonNamespaces, $options);
+        $blank = new DataBlank($this->commonNamespaces);
         $blank->initBySetResult($this->store->query('SELECT * WHERE {<'. $resourceUri .'> ?p ?o.}'), $resourceUri);
 
+        /*
+         * if its a proxy resource which inherits from another, get the properties of the other one.
+         */
         if (null !== $blank->get('kno:inheritsAllPropertiesOf')) {
             // get infos from the other resource
             $foreignResourceUri = $blank->get('kno:inheritsAllPropertiesOf');
             $foreignResourceInfo = $this->store->query('SELECT * WHERE {<'. $foreignResourceUri .'> ?p ?o.}');
-            $foreignResourceBlank = new DataBlank($this->commonNamespaces, $dataBlankOptions);
+            $foreignResourceBlank = new DataBlank($this->commonNamespaces);
             $foreignResourceBlank->initBySetResult($foreignResourceInfo, $foreignResourceUri);
             // copy property-value combination into blank instance
             foreach ($foreignResourceBlank as $property => $value) {
@@ -39,13 +41,16 @@ class Restriction
             }
         }
 
+        /*
+         * if its a list and the order of elements is fixed
+         */
         if (null !== $blank->get('kno:restrictionOrder')) {
             $orderResource = $blank->get('kno:restrictionOrder');
             $orderInformation = $this->store->query('SELECT * WHERE {'. $orderResource .' ?p ?o.}');
             $orderBlank = new DataBlank($this->commonNamespaces);
             $orderBlank->initBySetResult($orderInformation, $orderResource);
             $orderArray = $orderBlank->getArrayCopy();
-            ksort($orderArray);
+            ksort($orderArray); // sort by key
             $blank['kno:restrictionOrder'] = $orderArray;
         }
 

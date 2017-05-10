eb69a3c183099a0e46955b10a39326c53e362d44
Konrad Abicht <k.abicht@googlemail.com>
2017-05-10 19:47:31 +0200

    added assertStatementIteratorEquals + statementContainsNoBlankNodes
    to UnitTestCase with basic blank node support

--- tests/Knorke/UnitTestCase.php
+++ tests/Knorke/UnitTestCase.php
@@ -7,7 +7,9 @@ use PHPUnit\Framework\TestCase;
 use Saft\Rdf\CommonNamespaces;
 use Saft\Rdf\NodeFactoryImpl;
 use Saft\Rdf\RdfHelpers;
+use Saft\Rdf\Statement;
 use Saft\Rdf\StatementFactoryImpl;
+use Saft\Rdf\StatementIterator;
 use Saft\Rdf\StatementIteratorFactoryImpl;
 use Saft\Sparql\Query\QueryFactoryImpl;
 use Saft\Sparql\Result\SetResult;
@@ -127,5 +129,106 @@ class UnitTestCase extends TestCase
         } elseif (0 == count($actualEntriesNotFound) && 0 == count($notCheckedEntries)) {
             $this->assertEquals($expected->getVariables(), $actual->getVariables());
         }
+    }
+
+    /**
+     * Checks two lists which implements \Iterator interface, if they contain the same elements.
+     * The checks will be executed using PHPUnit's assert functions.
+     *
+     * @param StatementIterator $expected
+     * @param StatementIterator $actual
+     * @param boolean $debug optional, default: false
+     * @api
+     * @since 0.1
+     * @todo implement a more precise way to check blank nodes (currently we just count expected
+     *       and actual numbers of statements with blank nodes)
+     */
+    public function assertStatementIteratorEquals(
+        StatementIterator $expected,
+        StatementIterator $actual,
+        $debug = false
+    ) {
+        $entriesToCheck = array();
+        $expectedStatementsWithBlankNodeCount = 0;
+
+        foreach ($expected as $statement) {
+            // serialize entry and hash it afterwards to use it as key for $entriesToCheck array.
+            // later on we only check the other list that each entry, serialized and hashed, has
+            // its equal key in the list.
+            if (!$statement->isConcrete()) {
+                $this->markTestIncomplete('Comparison of variable statements in iterators not yet implemented.');
+            }
+            if ($this->statementContainsNoBlankNodes($statement)) {
+                $entriesToCheck[hash('sha256', $statement->toNQuads())] = false;
+            } else {
+                ++$expectedStatementsWithBlankNodeCount;
+            }
+        }
+
+        // contains a list of all entries, which were not found in $expected.
+        $actualEntriesNotFound = array();
+        $notCheckedEntries = array();
+        $foundEntries = array();
+        $actualStatementsWithBlankNodeCount = 0;
+
+        foreach ($actual as $statement) {
+            if (!$statement->isConcrete()) {
+                $this->markTestIncomplete("Comparison of variable statements in iterators not yet implemented.");
+            }
+            $statmentHash = hash('sha256', $statement->toNQuads());
+            // statements without blank nodes
+            if (isset($entriesToCheck[$statmentHash]) && $this->statementContainsNoBlankNodes($statement)) {
+                // if entry was found, mark it.
+                $entriesToCheck[$statmentHash] = true;
+                $foundEntries[] = $statement;
+
+            // handle statements with blank nodes separate because blanknode ID is random
+            // and therefore gets lost when stored (usually)
+            } elseif (false == $this->statementContainsNoBlankNodes($statement)) {
+                ++$actualStatementsWithBlankNodeCount;
+
+            // statement was not found
+            } else {
+                $actualEntriesNotFound[] = $statement;
+                $notCheckedEntries[] = $statement;
+            }
+        }
+
+        if (!empty($actualEntriesNotFound) || !empty($notCheckedEntries)) {
+            $message = 'The StatementIterators are not equal.';
+            if (!empty($actualEntriesNotFound)) {
+                if ($debug) {
+                    echo PHP_EOL . 'Following statements where not expected, but found: ';
+                    var_dump($actualEntriesNotFound);
+                }
+                $message .= ' ' . count($actualEntriesNotFound) . ' Statements where not expected.';
+            }
+            if (!empty($notCheckedEntries)) {
+                if ($debug) {
+                    echo PHP_EOL . 'Following statements where not present, but expected: ';
+                    var_dump($notCheckedEntries);
+                }
+                $message .= ' ' . count($notCheckedEntries) . ' Statements where not present but expected.';
+            }
+            $this->assertFalse(!empty($actualEntriesNotFound) || !empty($notCheckedEntries), $message);
+
+        // compare count of statements with blank nodes
+        } elseif ($expectedStatementsWithBlankNodeCount != $actualStatementsWithBlankNodeCount) {
+            $this->assertFalse(
+                true,
+                'Some statements with blank nodes where not found. '
+                    . 'Expected: ' . $expectedStatementsWithBlankNodeCount
+                    . 'Actual: ' . $actualStatementsWithBlankNodeCount
+            );
+
+        } else {
+            $this->assertTrue(true);
+        }
+    }
+
+    protected function statementContainsNoBlankNodes(Statement $statement) : bool
+    {
+        return false == $statement->getSubject()->isBlank()
+            && false == $statement->getObject()->isBlank();
     }
 }
